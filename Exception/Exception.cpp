7f59cd2c9f2be174b0ef97c6243cabc9459e2b81
h1nttt61 <Lyl1.32@yandex.ru>
2025-05-19 12:57:59 +0300

    Added nonexpepted and class Person with throw

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -1,21 +1,64 @@
 ﻿#include <iostream>
 
-double divide(int a, int b)
+class Person
 {
-    if (b)
-        return a / b;
-    throw "Cant deleted on 0";
+public:
+	Person(std::string name) : name{name}
+	{ 
+		std::cout << "Person created -> " << name << std::endl;
+	}
+	
+	~Person()
+	{
+		std::cout << "Person deleted ->" << name << std::endl;
+	}
+
+	void print()
+	{
+		throw "Print Error";
+	}
+private:
+	std::string name;
+};
+/*double divide(int a, int b)  //notexcept - willn't generate expeption
+{
+	if (b > a)
+		throw std::string("The second number is greater than the first one");
+	if (!b) // b == 0
+		throw 0;
+
+	return a / b;
 }
+
+void test(int a, int b)
+{
+	try
+	{
+		double result{ divide(a, b) };
+		std::cout << "Program fully worked, answer: " << result << std::endl;
+	}
+	catch (int code)
+	{
+		std::cout << "Error code: " << code << std::endl;
+	}
+	catch (std::string Error) //or (...). if without std::string{} if funcs -> const char* Error
+	{
+		std::cout << Error << std::endl;
+	}
+}*/
 
 void main()
 {
-    try
-    {
-        std::cout << divide(1, 2) << std::endl;
-        std::cout << "Program fully worked" << std::endl;
-    }
-    catch (...)
-    {
-        std::cout << "Error" << std::endl;
-    }
+	/*test(100, 20);
+	test(100, 0);
+	test(100, 1000);*/
+	try
+	{
+		Person person{ "Tom" };
+		person.print();
+	}
+	catch (const char* err)
+	{
+		std::cout << "Error: " << err << std::endl;
+	}
 }
