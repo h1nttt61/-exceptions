7b246f9821ddb6d67c98c7dbfec6ab874ca2cd79
h1nttt61 <Lyl1.32@yandex.ru>
2025-05-19 12:26:33 +0300

    Added throw

---
+++ Exception/Exception.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double divide(int a, int b)
+{
+    if (b)
+        return a / b;
+    throw "Cant deleted on 0";
+}
+
+void main()
+{
+    try
+    {
+        std::cout << divide(1, 2) << std::endl;
+        std::cout << "Program fully worked" << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "Error" << std::endl;
+    }
+}
